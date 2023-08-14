1952dae46741e71c27526f5b5b61bc56e540aa86
Carlos A. Novaez Guerrero <cnovaez.dev@gmail.com>
2023-08-14 19:06:05 -0300

    minor corrections

--- app/src/main/java/com/example/customdialoglibrary/MainActivity.kt
+++ app/src/main/java/com/example/customdialoglibrary/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.customdialoglibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cnovaez.dev.customizabledialoglibrary.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     }
 }
